8abcec7242e53a876934798ca5612ca4de43c6e1
TisBoyo <10230718+tisboyo@users.noreply.github.com>
2020-06-22 02:31:06 +0100

    Updated setup script

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y npm python3-venv
+sudo apt update
+sudo apt install -y npm libatlas-base-dev python3-venv
 
 python3 -m venv .venv
 source .venv/bin/activate
@@ -9,6 +9,14 @@ source .venv/bin/activate
 pip install -r requirements.txt
 
 cd web
-npm install
+# Build dmm.js
+# npm install
+# node_modules/.bin/browserify source.js -o dmm.js
 
-node_modules/.bin/browserify source.js -o dmm.js
+# Grab from Geekboy's server instead of building
+wget http://dmm.keepdream.in/dmm.js
+
+echo "******************************************************************************"
+echo "* Make sure to create a settings.py {use settings.py.template as a template} *"
+echo "* To start use: source ./run.sh                                              *"
+echo "******************************************************************************"
