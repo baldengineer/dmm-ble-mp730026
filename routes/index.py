927f1a0587be9c702efec6d54562d74ad1954bd5
TisBoyo <10230718+tisboyo@users.noreply.github.com>
2020-08-02 20:30:32 +0100

    more work on fastapi

--- routes/index.py
+++ routes/index.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, WebSocket
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse, JSONResponse
+import uvicorn
 import asyncio
 import websockets
 import logging
@@ -13,17 +14,33 @@ logger = logging.getLogger("app")
 @router.get("/")
 @router.get("/index.html")
 async def root():
-    # return FileResponse("web/index.html")
     return HTMLResponse(
         '<html><body><center><img src="https://www.inzonedesign.com/wp-content/uploads/2016/10/blog-header-astronaut-houston-we-have-a-problem-im-lost.jpg" /></center></body></html>'
     )
 
 
-# @router.get("/dmm.js")
-# async def send_dmmjs():
-#     return FileResponse("web/dmm.js", media_type="text/javascript")
+@router.get("/favicon.ico")
+async def favicon():
+    return FileResponse("web/favicon.ico")
 
 
-# @router.get("/segment-display.js")
-# async def send_segment_displayjs():
-#     return FileResponse("web/segment-display.js", media_type="text/javascript")
+@router.get("/dmm.js")
+async def send_dmmjs():
+    return FileResponse("web/dmm.js", media_type="text/javascript")
+
+
+@router.get("/segment-display.js")
+async def send_segment_displayjs():
+    return FileResponse("web/segment-display.js", media_type="text/javascript")
+
+
+@router.get("/meter.html")
+async def meter():
+    return FileResponse("web/meter.html", media_type="text/html")
+
+
+@router.get("/{item_id}")
+async def live_meter(item_id: str):
+    # url = f"http://192.168.1.71:18881/meter.html?websocketserver=192.168.1.71&websocketport=18881&meter={item_id}"
+    # return RedirectResponse(url)
+    pass
