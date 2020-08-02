06ce950b6ed6212a951c4c92a75109ae9c322c02
TisBoyo <10230718+tisboyo@users.noreply.github.com>
2020-08-03 00:53:51 +0100

    fastapi working

--- routes/index.py
+++ routes/index.py
@@ -10,15 +10,21 @@ import settings  # Meter settings
 router = APIRouter()
 logger = logging.getLogger("app")
 
+# Default colors that are passed when using the redirects
+default_oncolor = "rgb(250,250,164,.9)"
+default_offcolor = "rgb(16,32,16,.5)"
+default_background = "rgb(0,0,0,.5)"
+default_text = "%20"
+
 
 @router.get("/")
-@router.get("/index.html")
 async def root():
     return HTMLResponse(
         '<html><body><center><img src="https://www.inzonedesign.com/wp-content/uploads/2016/10/blog-header-astronaut-houston-we-have-a-problem-im-lost.jpg" /></center></body></html>'
     )
 
 
+# Favicon just because
 @router.get("/favicon.ico")
 async def favicon():
     return FileResponse("web/favicon.ico")
@@ -34,23 +40,60 @@ async def send_segment_displayjs():
     return FileResponse("web/segment-display.js", media_type="text/javascript")
 
 
+# The actual route to load the meter
 @router.get("/meter")
 async def meter():
     return FileResponse("web/meter.html", media_type="text/html")
 
 
+# Route to load saved data
 @router.get("/saved")
 async def saved():
     return FileResponse("web/saved.html", media_type="text/html")
 
 
+# A redirect to access live data based on meter id
 @router.get("/{item_id}")
-async def live_meter(item_id: str, request: Request):
-    url = f"{request.base_url}meter?websocketserver={request.base_url.hostname}&websocketport={request.base_url.port}&meter={item_id}"
+async def live_meter(
+    item_id: str,
+    request: Request,
+    offcolor: str = default_offcolor,
+    oncolor: str = default_oncolor,
+    background: str = default_background,
+    text: str = default_text,
+):
+    url = (
+        f"{request.base_url}meter?"
+        f"websocketserver={request.base_url.hostname}"
+        f"&websocketport={request.base_url.port}"
+        f"&meter={item_id}"
+        f"&onColor={oncolor}"
+        f"&offColor={offcolor}"
+        f"&background={background}"
+        f"&displaytxt={text}"
+    )
+
     return RedirectResponse(url)
 
 
+# A redirect to access saved data based on meter id
 @router.get("/saved/{item_id}")
-async def saved_meter(item_id: str, request: Request):
-    url = f"{request.base_url}saved?websocketserver={request.base_url.hostname}&websocketport={request.base_url.port}&meter={item_id}"
+async def saved_meter(
+    item_id: str,
+    request: Request,
+    offcolor: str = default_offcolor,
+    oncolor: str = default_oncolor,
+    background: str = default_background,
+    text: str = default_text,
+):
+    url = (
+        f"{request.base_url}saved"
+        f"?websocketserver={request.base_url.hostname}"
+        f"&websocketport={request.base_url.port}"
+        f"&meter={item_id}"
+        f"&oncolor={oncolor}"
+        f"&offcolor={offcolor}"
+        f"&background={background}"
+        f"&displaytxt={text}"
+    )
     return RedirectResponse(url)
